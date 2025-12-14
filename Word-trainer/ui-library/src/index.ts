5cecce03a04cb0b1dbb07bd3ae1bc2ce8dc4f6fb
asfkam1ru-hub <asfkam1ru@gmail.com>
2025-12-15 00:03:31 +0300

    hot fixes

--- Word-trainer/ui-library/src/index.ts
+++ Word-trainer/ui-library/src/index.ts
@@ -1,4 +1,15 @@
-export { Layout } from './components/Layout'
+export type * from './types'
+
+// Components
 export { DeckList } from './components/DeckList'
-export type { DeckListProps, DeckListItem } from './components/DeckList'
+export { DecksPanel } from './components/DecksPanel'
+export { FlashcardTrainer } from './components/FlashcardTrainer'
+export { Layout } from './components/Layout'
+export { WordDetails } from './components/WordDetails'
+export { WordList } from './components/WordList'
 export { WordOfTheDayWidget } from './components/WordOfTheDayWidget'
+
+// Utils
+export function buildButtonLabel(label: string): string {
+  return label
+}
